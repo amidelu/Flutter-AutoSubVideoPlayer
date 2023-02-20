6792f8c715e3baffd43a4f6da4b844382038ee70
amidelu <delowar_gtc@yahoo.com>
2023-02-20 17:30:17 +0600

    packages and dynamic material3 added

---
+++ android/app/src/main/java/com/andro4everyone/sub_video_player/MainActivity.java
@@ -0,0 +1,6 @@
+package com.andro4everyone.sub_video_player;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:dynamic_color/dynamic_color.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+const _brandColor = Color(0xFF44CD8D);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        ColorScheme lightColorScheme;
+        ColorScheme darkColorScheme;
+        if (lightDynamic != null && darkDynamic != null) {
+          lightColorScheme = lightDynamic.harmonized();
+          darkColorScheme = darkDynamic.harmonized();
+        } else {
+          lightColorScheme = ColorScheme.fromSeed(seedColor: _brandColor);
+          darkColorScheme = ColorScheme.fromSeed(
+              seedColor: _brandColor, brightness: Brightness.dark);
+        }
+        return MaterialApp(
+          title: 'Auto Sub Player',
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: lightColorScheme,
+          ),
+          darkTheme: ThemeData(
+            useMaterial3: true,
+            colorScheme: darkColorScheme,
+          ),
+          // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        );
+      },
+    );
+  }
+}
