065c7cd6227a89506cebf5c4cbd8d7a5212fb423
amidelu <delowar_gtc@yahoo.com>
2023-02-22 18:03:36 +0600

    internal storage video files loaded

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:sub_video_player/util/logger.dart';
 
 class Dashboard extends StatefulWidget {
@@ -14,24 +15,70 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   // Getting video file path
   List<FileSystemEntity> _files = [];
+  List<String> ext = [
+    ".3g2",
+    ".3gp",
+    ".asf",
+    ".asx",
+    ".avi",
+    ".flv",
+    ".m2ts",
+    ".mkv",
+    ".mov",
+    ".mp4",
+    ".mpg",
+    ".mpeg",
+    ".rm",
+    ".swf",
+    ".vob",
+    ".wmv"
+  ];
 
   void _getFiles() async {
-    Directory? directory = await getDownloadsDirectory();
-    List<FileSystemEntity> files = directory!
+    // List<Directory>? directories = await getExternalStorageDirectories();
+    final Directory root = Directory('/storage/emulated/0/');
+    final Directory? external = await getExternalStorageDirectory();
+    logger.i('External Storage path', external);
+
+    List<FileSystemEntity> files = root
         .listSync(recursive: true)
-        .where((element) => element.path.endsWith('.mp4'))
+        .where((element) =>
+            element.path.endsWith('.mp4') || element.path.endsWith('.mkv'))
         .toList();
 
+    List<FileSystemEntity> externalFiles = [];
+    if (external != null) {
+      externalFiles = external
+          .listSync(recursive: true)
+          .where((element) =>
+              element.path.endsWith('mp4') || element.path.endsWith('mkv'))
+          .toList();
+    }
+
     setState(() {
-      _files = files;
+      _files = [...files, ...externalFiles];
       logger.d('file path', _files);
     });
+  }
+
+  void _checkPermission() async {
+    if (await Permission.storage.request().isGranted) {
+      _getFiles();
+    } else {
+      await Permission.storage.request().then((value) {
+        if (value.isGranted) {
+          _getFiles();
+        } else if (value.isDenied) {
+          // show alert that permission is necessary
+        }
+      });
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    _getFiles();
+    _checkPermission();
   }
 
   @override
